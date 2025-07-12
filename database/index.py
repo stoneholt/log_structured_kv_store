c66bf527779d425f6fd284d70ac7a277aa3c8360
Jeff Rubillo <jrubillo@gmail.com>
2025-07-12 12:18:03 -0400

    More index functions

--- database/index.py
+++ database/index.py
@@ -1,5 +1,11 @@
+from typing import List
+
 
 class Index:
+    """
+        Creates a sorted search structure used for querying
+    """
+
     def __init__(self):
         raise NotImplementedError()
 
@@ -7,4 +13,13 @@ class Index:
         raise NotImplementedError()
 
     def remove_from_index(self, key: str):
+        raise NotImplementedError()
+
+    def search(self, key: str):
+        raise NotImplementedError()
+
+    def range_query(self, start: str, end: str) -> List:
+        raise NotImplementedError()
+
+    def starts_with(self, query_string: str):
         raise NotImplementedError()
