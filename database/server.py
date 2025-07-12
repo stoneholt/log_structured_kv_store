cf4b8a5a47fce634c9165e8e3a124663fce7ee8e
Jeff Rubillo <jrubillo@gmail.com>
2025-07-12 19:31:51 -0400

    database server interface mock

--- database/server.py
+++ database/server.py
@@ -1,28 +1,87 @@
 from fastapi import FastAPI
+from .index import Index
 # from .keydir import KeyDir
 from pydantic import BaseModel
+
+from abc import ABC, abstractmethod
+
+from fastapi import FastAPI
+
 app = FastAPI()
+
+
+class DataStore(ABC):
+    @abstractmethod
+    def put(self, key, value):
+        pass
+
+    @abstractmethod
+    def get(self, key):
+        pass
+
+    @abstractmethod
+    def delete(self, key):
+        pass
+
+    @abstractmethod
+    def prefix_query(self, query):
+        pass
+
+
+class SimpleDataStore(DataStore):
+    def __init__(self):
+        self.data = {}
+        self.index = Index()
+        pass
+
+    def get(self, key):
+        if key in self.data:
+            return self.data[key]
+        return None
+
+    def put(self, key, value):
+        self.index.add_to_index(key)
+        self.data[key] = value
+
+    def delete(self, key):
+        if key in self.data:
+            del self.data[key]
+            self.index.remove_from_index(key)
+
+    def prefix_search(self, query):
+        keys = self.index.starts_with(query)
+        results = []
+        for key in keys:
+            results.append((key, self.data[key]))
+        return results
 
 
 class Payload(BaseModel):
     data: str
 
 
+store = SimpleDataStore()
+
+
 @app.get("/v1/{key}")
 def get_key(key: str):
-    return {"message": "placeholder"}
+
+    return {"data": store.get(key)}
 
 
 @app.put("/v1/{key}")
 def put_key(key: str, body: Payload):
-    return {"message": "payload"}
+    store.put(key, body.data)
+    return {"data": {"key": key, "body": body.data}}
 
 
 @app.delete("/v1/{key}")
 def delete_key(key: str):
-    return {"message": "delete"}
+    store.delete(key)
+    return {"data": "deleted"}
 
 
 @app.get("/v1/prefix/{prefix}")
 def prefix_search(prefix: str):
-    return {"message": "prefix"}
+    results = store.prefix_search(prefix)
+    return {"data": results}
