c2347e0266a6366f75caa55d253eb69fdfbe6aa5
Jeff Rubillo <jrubillo@gmail.com>
2025-07-12 14:34:27 -0400

    Prefix search for index

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,3 @@
-import pytest
-
 from database.index import Index
 
 
@@ -8,12 +6,12 @@ def test_basic_index():
     idx.add_to_index(4)
     idx.add_to_index(5)
 
-    assert idx.search(4) == 4
-    assert idx.search(5) == 5
-    assert idx.search(6) == None
+    assert 4 == idx.search(4)
+    assert 5 == idx.search(5)
+    assert None == idx.search(6)
 
     idx.remove_from_index(5)
-    assert idx.search(5) == None
+    assert None == idx.search(5)
 
 
 def test_index_with_strings():
@@ -23,14 +21,18 @@ def test_index_with_strings():
     idx.add_to_index("cheese")
     idx.add_to_index("dog")
 
-    assert idx.search("cheese") == "cheese"
+    assert "cheese" == idx.search("cheese")
 
 
-def test_index_with_strings():
+def test_prefix_search():
     idx = Index()
-    idx.add_to_index("apple")
-    idx.add_to_index("banana")
+    idx.add_to_index("alfred")
     idx.add_to_index("cheese")
+    idx.add_to_index("cheddar")
+    idx.add_to_index("chew")
+    idx.add_to_index("abchew")
     idx.add_to_index("dog")
-    idx.keylist.display()
-    assert 1 == 2
+    idx.add_to_index("zebra")
+    idx.add_to_index("cha")
+
+    assert ["cheddar", "cheese", "chew"] == idx.starts_with("che")
